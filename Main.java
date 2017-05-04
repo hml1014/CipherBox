3740087151d87346d60e83c639e762db44405821
Hannah <hml1014@sbcglobal.net>
2017-05-03 22:43:29 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 package ciphers;
 
+/* Hannah Lehr
+ * 2017
+ * 
+ * This main class launches the CipherBox gui.
+ */
+
 public class Main {
 	public static void main(String[] args) {
-		CipherBox cb = new CipherBox();	
+		new CipherBox();			
 	}
+
 }
